ea30278813203a52a0f04ec495d1e1ab35e92e7b
bouvet-inge-halvorsen <inge.halvorsen@bouvet.no>
2024-10-10 14:47:55 +0200

    adds test that will fail on assertion

--- test_library_pytest.py
+++ test_library_pytest.py
@@ -13,9 +13,13 @@ def library() -> Library:
 
 def test_add_book(library, book_stack):
     """Tests that a book can be added to the library"""
-    library.add_book(book_stack[0])
+    assert library.add_book(book_stack[0]) == True
 
     assert library.library_files[0] == book_stack[0]
+
+#TODO: should be moved to tests for books or something
+def test_can_get_isbn_from_book(book_stack):
+    assert book_stack[0].get_isbn() == "123-123-123-0"
 
 def test_can_delete_a_book(library, book_stack):
     # assert that the library is empty
@@ -25,5 +29,27 @@ def test_can_delete_a_book(library, book_stack):
     library.add_book(book_stack[0])
     library.add_book(book_stack[1])
 
-    #assert that the data is in the library
+    # assert that the data is in the library
     assert len(library.library_files) == 2
+
+    # removing a book will return a bool, true if book was removed
+    assert library.remove_book(book_stack[0].get_isbn()) == True
+    
+    # with the book deleted, teher should be 1 book left
+    assert len(library.library_files) == 1
+
+    # and there should be no possibility to delete the prevous book
+    assert library.remove_book(book_stack[0].get_isbn()) == False
+
+def test_should_not_be_able_to_add_a_book_that_already_exists(library, book_stack):
+    # assert that the library is empty
+    assert len(library.library_files) == 0
+
+    # add a book to the library
+    assert library.add_book(book_stack[0]) == True
+
+    # assert that the book is in the library
+    assert library.get_whole_library()[0] == book_stack[0].pretty_print_book()
+
+    # If the book exists, return False
+    assert library.add_book(book_stack[0]) == False
